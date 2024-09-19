05e30b5de77418b92e55c85c7e1e192e108dc1f6
DinhAnh2301 <dinhanh2301@gmail.com>
2024-09-19 13:32:29 -0600

    week-3 submitted

--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="./week-2/">Week 2 Assignment</Link>
+      <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2/" className="block">Week 2 Assignment</Link>
+      <Link href="./week-3/" className="block">Week 3 Assignment</Link>
     </main>
   );
 }
---
+++ app/week-3/item.js
@@ -0,0 +1,10 @@
+
+
+export default function Item({name, quantity, category}) {
+    return (
+        <div className="bg-cyan-950 w-1/3 m-4 p-2">
+            <h2 className="text-xl font-bold">{name}</h2>
+            <p>Buy {quantity} in {category}</p>
+        </div>
+    );
+}
