5abb18e231c76160ad034b6cddda2ca9ed2e4a9f
DinhAnh2301 <dinhanh2301@gmail.com>
2024-10-13 17:03:02 -0600

    week completed

--- app/page.js
+++ app/page.js
@@ -3,11 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="m-10">
+    <main className="flex m-20 p-10">
+      <div>
       <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="./week-2/" className="block hover:underline hover:text-green-500">Week 2 Assignment</Link>
       <Link href="./week-3/" className="block hover:underline hover:text-green-500">Week 3 Assignment</Link>
       <Link href="./week-4/" className="block hover:underline hover:text-green-500">Week 4 Assignment</Link>
+      <Link href="./week-5/" className="block hover:underline hover:text-green-500">Week 5 Assignment</Link>
+      </div>
     </main>
   );
 }
---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <main className="flex justify-center min-h-screen bg-black">
+            <div>
+                <NewItem />
+            </div>
+        </main>
+    );
+}
