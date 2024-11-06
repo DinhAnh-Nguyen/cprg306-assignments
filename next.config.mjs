06ddc2daf1c38f9140ad1c3d37bc5899ad6dbe3b
DinhAnh2301 <dinhanh2301@gmail.com>
2024-11-05 19:00:22 -0700

    week-8 assignment completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
- 
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    /* config options here */
-  }
-   
-  export default nextConfig
+  images: {
+    domains: ["www.themealdb.com"],
+  },
+};
+
+export default nextConfig;
